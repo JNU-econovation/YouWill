efa9314937cef84315d522954f0c33955ea39504
asong57 <asong57@naver.com>
2021-05-26 16:36:39 +0900

    feat: initial commit

---
+++ app/src/main/java/com/kangaroos/youwill/MyPage.java
@@ -0,0 +1,49 @@
+package com.kangaroos.youwill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MyPage extends AppCompatActivity {
+    private FirebaseAuth mAuth ;
+    Button buttonLogout, buttonExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogout = (Button) findViewById(R.id.button_logout);
+        buttonExit = (Button) findViewById(R.id.button_exit);
+
+        buttonLogout.setOnClickListener(this::onClick);
+        buttonExit.setOnClickListener(this::onClick);
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    private void revokeAccess() {
+        mAuth.getCurrentUser().delete();
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_logout:
+                signOut();
+                finishAffinity();
+                break;
+            case R.id.button_exit:
+                revokeAccess();
+                finishAffinity();
+                break;
+        }
+    }
+}
