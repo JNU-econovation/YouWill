e1e040dc77359bf47204e0e5b4f6d390529961b1
unknown <gkminju2000@gmail.com>
2021-05-26 16:58:35 +0900

    feat: will레이아웃추가

---
+++ app/src/main/java/com/kangaroos/youwill/Will.java
@@ -0,0 +1,14 @@
+package com.kangaroos.youwill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Will extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_will);
+    }
+}
