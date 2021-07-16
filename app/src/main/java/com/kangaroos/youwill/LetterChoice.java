fca6c788a3e651b8a4a9da81db8b4c2723436a3d
unknown <gkminju2000@gmail.com>
2021-07-16 17:38:47 +0900

    feat: 텍스트 라이브러리 적용

--- app/src/main/java/com/kangaroos/youwill/LetterChoice.java
+++ app/src/main/java/com/kangaroos/youwill/LetterChoice.java
@@ -3,25 +3,33 @@ package com.kangaroos.youwill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+<<<<<<< Updated upstream
 import android.os.Bundle;
 import android.app.Activity;
+=======
+>>>>>>> Stashed changes
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class LetterChoice extends AppCompatActivity {
-    Button button_sub;
+
+    Button button_main;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letter_choice);
 
-        button_sub = findViewById(R.id.button_sub);
+        button_main = findViewById(R.id.button_main);
+        ImageView imageView = (ImageView)findViewById(R.id.imageview1);
+        TextView textView = (TextView)findViewById(R.id.textview1);
 
-        button_sub.setOnClickListener(new View.OnClickListener() {
+        imageView.setImageResource(R.drawable.image1);
+        button_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LetterWrite.class);
--- app/src/main/java/com/kangaroos/youwill/LetterWrite.java
+++ app/src/main/java/com/kangaroos/youwill/LetterWrite.java
@@ -8,17 +8,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< Updated upstream
 
+=======
+import android.widget.ImageView;
+import android.widget.TextView;
+>>>>>>> Stashed changes
 
 
 public class LetterWrite extends AppCompatActivity {
     Button button_sub;
 
     @Override
+<<<<<<< Updated upstream
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letter_write);
 
+=======
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_letter_write);
+
+        button_sub = findViewById(R.id.button_sub);
+        ImageView imageView = (ImageView)findViewById(R.id.imageview1);
+        TextView textView = (TextView)findViewById(R.id.textview1);
+
+        imageView.setImageResource(R.drawable.image1);
+        button_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LetterWrite.class);
+                startActivity(intent);
+            }
+        });
+>>>>>>> Stashed changes
 
     }
 
