03d0d8755251e5d77387f8c370adcb0cddd2800b
unknown <gkminju2000@gmail.com>
2021-07-20 18:39:06 +0900

    feat : willitem 추가

---
+++ app/src/main/java/com/kangaroos/youwill/WillItem.java
@@ -0,0 +1,32 @@
+package com.kangaroos.youwill;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class WillItem  {
+    String date;
+    String content;
+
+    public WillItem() {
+    }
+
+    public WillItem(String date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    protected WillItem(Parcel in) {
+        content = in.readString();
+        date = in.readString();
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
