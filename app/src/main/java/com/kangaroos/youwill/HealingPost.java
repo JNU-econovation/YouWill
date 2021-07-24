07f42a08bef98df9554af032515cc0d345ea3c04
asong57 <1998thddk@gmail.com>
2021-07-25 01:32:08 +0900

    fix : merge 오류

--- app/src/main/java/com/kangaroos/youwill/HealingPost.java
+++ app/src/main/java/com/kangaroos/youwill/HealingPost.java
@@ -11,33 +11,29 @@ import java.util.ArrayList;
 
 public class HealingPost extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_healing_post);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_healing_post);
 
         Intent intent = getIntent();
+
 
         String title = intent.getExtras().getString("title");
         String content = intent.getExtras().getString("content");
         String date = intent.getExtras().getString("date");
         int likesCount = intent.getExtras().getInt("likesCount");
 
-            String title = intent.getExtras().getString("title");
-            String content = intent.getExtras().getString("content");
-            String date = intent.getExtras().getString("date");
-            int likesCount = intent.getExtras().getInt("likesCount");
+        TextView textView_title = findViewById(R.id.textView_post_title);
+        TextView textView_content = findViewById(R.id.textView_post_content);
+        TextView textView_date = findViewById(R.id.textView_post_date);
+        TextView textView_number = findViewById(R.id.textView_post_number);
 
-            TextView textView_title = findViewById(R.id.textView_post_title);
-            TextView textView_content = findViewById(R.id.textView_post_content);
-            TextView textView_date = findViewById(R.id.textView_post_date);
-            TextView textView_number = findViewById(R.id.textView_post_number);
+        textView_title.setText(title);
+        textView_content.setText(content);
+        textView_date.setText(date);
+        textView_number.setText(String.valueOf(likesCount));
 
-            textView_title.setText(title);
-            textView_content.setText(content);
-            textView_date.setText(date);
-            textView_number.setText(String.valueOf(likesCount));
-          
         textView_title.setText(title);
         textView_content.setText(content);
         textView_date.setText(date);
