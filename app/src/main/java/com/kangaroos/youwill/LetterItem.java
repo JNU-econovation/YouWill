6efb9a87ca62b13aa3b540f32a985675678463f0
unknown <gkminju2000@gmail.com>
2021-07-26 18:13:56 +0900

    feat : button이미지조절, letter db 연결중

---
+++ app/src/main/java/com/kangaroos/youwill/LetterItem.java
@@ -0,0 +1,41 @@
+package com.kangaroos.youwill;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class LetterItem  {
+    String recipient_number;
+    String write_date;
+    String send_date;
+    int paper_type;
+    String content;
+
+
+    public LetterItem() {
+    }
+
+    public LetterItem(String recipient_number, String write_date, String send_date, int paper_type, String content) {
+        this.recipient_number = recipient_number;
+        this.write_date = write_date;
+        this.send_date = send_date;
+        this.paper_type = paper_type;
+        this.content = content;
+
+
+    }
+
+    protected LetterItem(Parcel in) {
+        recipient_number = in.readString();
+        write_date = in.readString();
+        send_date = in.readString();
+        paper_type = in.readInt();
+        content = in.readString();
+    }
+
+
+    public String getRecipient_number() { return recipient_number; }
+    public String getWrite_date() { return write_date; }
+    public String getSend_date() { return send_date; }
+    public int getPaper_type() { return paper_type; }
+    public String getContent() { return content; }
+}
